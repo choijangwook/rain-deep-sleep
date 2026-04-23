93186180cd32ad0d42705c8a52bb7ef33156a959
choi jang wook <jangwookchoi1@gmail.com>
2026-04-23 18:03:25 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,35 @@
-console.log("Rain Deep Sleep loaded");
+const audio = document.getElementById("rainAudio");
+
+function playRain() {
+    audio.volume = 0;
+    audio.play();
+
+    // 🌿 fade in
+    let v = 0;
+    let fade = setInterval(() => {
+        if (v < 1) {
+            v += 0.05;
+            audio.volume = v;
+        } else {
+            clearInterval(fade);
+        }
+    }, 100);
+}
+
+function pauseRain() {
+    // 🌿 fade out
+    let v = audio.volume;
+    let fade = setInterval(() => {
+        if (v > 0) {
+            v -= 0.05;
+            audio.volume = v;
+        } else {
+            audio.pause();
+            clearInterval(fade);
+        }
+    }, 100);
+}
+
+function setVolume(val) {
+    audio.volume = val;
+}
